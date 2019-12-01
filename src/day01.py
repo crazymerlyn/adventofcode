fcac147d0223737163a45d249602e5b18c9b6bc7
Ankit Goel <ankitgoel616@gmail.com>
2019-12-01 13:41:49 +0000

    Initial setup and problem 1

---
+++ src/day01.py
@@ -0,0 +1,11 @@
+def fuel(x):
+    return x // 3 - 2
+
+def total_fuel(x):
+    return 0 if x <= 6 else fuel(x) + total_fuel(fuel(x))
+
+def part1(lines):
+    return sum(fuel(int(line)) for line in lines)
+
+def part2(lines):
+    return sum(total_fuel(int(line)) for line in lines)
